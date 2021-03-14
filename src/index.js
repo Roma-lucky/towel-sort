b1d5c9eafb21e0d92f4c84a5970132e42aa900ef
Roman <roman.blyk@gmail.com>
2021-03-14 17:34:19 +0200

    feat: create function towelSort

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+  
+matrix = matrix.join().split(',').sort((a,b) => a-b)
+matrix = matrix.map(Number);
+  let size = 3;
+  let subarray = []
+    for (let i = 0; i <Math.ceil(matrix.length/size); i++){
+      subarray[i] = matrix.slice((i*size), (i*size) + size);
+    }
+    for(i=0; i<subarray.length; i++) {
+        i%2?false:subarray[i].reverse()
+            for(k=0;k<subarray.length;k++) {
+                i%1?false:subarray[i].reverse()}
+    }
+    if(typeof matrix === 'undefined' || matrix.length == 0) {
+      return [];
+    }
+
+    return subarray.join().split(',').map(Number);
+};
